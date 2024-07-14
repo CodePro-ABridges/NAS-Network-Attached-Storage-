b612d7fc40803fe6cadf0cd82e9e85f5b935857b
CodePro-ABridges <arthurbridges08@yahoo.com>
2024-07-13 21:56:41 -0500

    ADDITION: Tailwind css config

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "custom-blue": "#1fb6ff",
+      },
+    },
+  },
+  plugins: [],
+};
